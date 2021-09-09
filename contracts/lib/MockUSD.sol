6c114c53464a7245cdfd5399d43bf51abaa3b261
ZZ <490396674@qq.com>
2021-09-09 12:50:56 +0800

    update compile sucess code

---
+++ contracts/lib/MockUSD.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSD is ERC20 {
+    address public minter;
+    address public owner;
+    event MinterChanged(address indexed from, address to);
+
+    /**
+     * @notice use ERC20 constructor and set the owner
+     */
+    constructor() payable ERC20("MockUSD", "MockUSD") {
+        minter = msg.sender;
+        owner = msg.sender;
+    }
+
+    /**
+     * @notice get the balance that one user(LP) can unlock(maximum)
+     * @param _newMinter: new minter's address
+     */
+    function passMinterRole(address _newMinter) public returns (bool) {
+        require(
+            msg.sender == owner,
+            "Error! only the owner can change the minter role"
+        );
+        minter = _newMinter;
+
+        emit MinterChanged(msg.sender, _newMinter);
+        return true;
+    }
+
+    /**
+     * @notice mint _amount tokens to _account
+     * @param _account: receiver's address
+     * @param _amount: amount to be minted
+     */
+    function mint(address _account, uint256 _amount) public {
+        //check if msg.sender have minter role
+        require(msg.sender == minter, "Error! Msg.sender must be the minter");
+
+        _mint(_account, _amount);
+    }
+}
